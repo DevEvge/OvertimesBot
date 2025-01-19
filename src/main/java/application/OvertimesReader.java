7e9eee6f0582704f295e5547518339947b2ce1bb
DevEvge <evgeniivolkov18@gmail.com>
2025-01-20 00:59:26 +0200

    Create method overtimesForThisMonth

---
+++ src/main/java/application/OvertimesReader.java
@@ -0,0 +1,43 @@
+package application;
+
+import DataBaseController.DataBaseConnection;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class OvertimesReader {
+    DataBaseConnection database = new DataBaseConnection();
+
+
+    private Double overtimesForThisMonth(long chatID) {
+        MongoCollection<Document> collection = database.connectDatabase();
+
+        LocalDate date = LocalDate.now();
+        LocalDate startOfMouth = date.withDayOfMonth(1);
+        LocalDate endOfMouth = startOfMouth.plusMonths(1);
+
+        String startDataStr = startOfMouth.toString();
+        String endDataStr = endOfMouth.toString();
+
+        List<Document> pipeline = Arrays.asList(
+                new Document("$Match", new Document("ChatID", chatID)
+                        .append("Date", new Document("$gte", startDataStr)
+                                .append("$lt", endDataStr))),
+                new Document("$group", new Document("_id", "$ChatID")
+                        .append("totalHours", new Document("$sum", new Document("toDouble", "$hours"))))
+        );
+
+        AggregateIterable<Document> result = collection.aggregate(pipeline);
+        Document aggregatedResult = result.first();
+
+        if (aggregatedResult != null && aggregatedResult.get("totalHours") != null) {
+            return aggregatedResult.getDouble("totalHours");
+        }
+        return 0.0;
+    }
+}
